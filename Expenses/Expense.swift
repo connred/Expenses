9492ead1f582b6c076787b32ce4e587f20fc60ed
Connor Redmond <Connor Redmond>
2020-09-08 16:01:21 -0500

    Implemented expenses

---
+++ Expenses/Expense.swift
@@ -0,0 +1,15 @@
+//
+//  Expense.swift
+//  Expenses
+//
+//  Created by cdrm9t on 9/8/20.
+//  Copyright © 2020 cdrm9t. All rights reserved.
+//
+
+import Foundation
+
+struct Expense {
+    let title : String
+    let amount : Double
+    let date : Date
+}
